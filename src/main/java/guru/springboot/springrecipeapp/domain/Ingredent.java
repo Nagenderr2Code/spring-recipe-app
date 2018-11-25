017fd85835f481bf6b2d791a14799ad5235263c0
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-11-25 16:00:09 -0500

    Adding Recipe, Notes, Ingredient

---
+++ src/main/java/guru/springboot/springrecipeapp/domain/Ingredent.java
@@ -0,0 +1,50 @@
+package guru.springboot.springrecipeapp.domain;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal quantity;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
---
+++ src/main/java/guru/springboot/springrecipeapp/domain/Notes.java
@@ -0,0 +1,35 @@
+package guru.springboot.springrecipeapp.domain;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
