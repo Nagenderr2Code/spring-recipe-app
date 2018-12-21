0ae476802119e5dbd690f516fecc64f3d4136d25
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-12-21 15:55:51 -0500

    Spring Security with InMemory Authentication Closes #34

--- src/main/java/guru/springboot/springrecipeapp/RecipeAppConfig.java
+++ src/main/java/guru/springboot/springrecipeapp/RecipeAppConfig.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class RecipeAppConfig{
+public class RecipeAppConfig implements WebMvcConfigurer {
 
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 
     @Bean(name = "conversionService")
-    public ConversionService conversionService(){
+    public ConversionService conversionService() {
         DefaultConversionService conversionService = new DefaultConversionService();
         conversionService.addConverter(new IngredientConverter(conversionService));
         conversionService.addConverter(new NotesConverter(conversionService));
---
+++ src/main/java/guru/springboot/springrecipeapp/security/AppSecurityConfig.java
@@ -0,0 +1,36 @@
+package guru.springboot.springrecipeapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("nagender").password("nagender").roles("COOK"))
+                .withUser(userBuilder.username("soujanya").password("soujanya").roles("MANAGER"))
+                .withUser(userBuilder.username("higreev").password("higreev").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
