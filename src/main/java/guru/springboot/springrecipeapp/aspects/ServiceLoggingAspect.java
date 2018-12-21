d4dc233e9ab563cc0b183efd54dfe707dfa0a687
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-12-21 13:17:55 -0500

    @AfterThrownig Closes #32

--- src/main/java/guru/springboot/springrecipeapp/aspects/ServiceLoggingAspect.java
+++ src/main/java/guru/springboot/springrecipeapp/aspects/ServiceLoggingAspect.java
@@ -1,10 +1,16 @@
 package guru.springboot.springrecipeapp.aspects;
 
 
+import guru.springboot.springrecipeapp.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -12,7 +18,18 @@ import org.springframework.stereotype.Component;
 public class ServiceLoggingAspect {
 
     @AfterReturning(pointcut = "guru.springboot.springrecipeapp.aspects.PointCutDeclaration.serviceLogging()", returning = "result")
-    public void serviceLogging(Object result){
-        log.info("Aspect Logging For Service" + result.toString());
+    public void serviceLogging(JoinPoint joinPoint, Object result){
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        joinPoint.getArgs();
+        joinPoint.getTarget();
+        log.info("Aspect Logging For Service" + methodSignature.getName() + result.toString());
+    }
+
+    @AfterThrowing(pointcut = "guru.springboot.springrecipeapp.aspects.PointCutDeclaration.serviceLogging()", throwing = "ex")
+    public void logException(JoinPoint joinPoint, NotFoundException ex){
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String args = Arrays.toString(joinPoint.getArgs());
+        log.error("Logging from Aspect.." + ex.getMessage() + methodSignature.toString() + args);
     }
 }
